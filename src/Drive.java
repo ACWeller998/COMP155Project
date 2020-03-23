4bc2f1f350846369d3816d285c2500ab4be727bd
MidnightWolfe <36086769+MidnightWolfe@users.noreply.github.com>
2020-03-23 14:22:32 -0700

    Update Drive.java

--- src/Drive.java
+++ src/Drive.java
@@ -1,2 +1,65 @@
-public class Drive {
+
+  public class Drive {
+	
+	private int pos;
+	private String direct;
+	int current = 1;
+	
+	/* The method setMovement stores a value in the fields
+	 * direct and pos.
+	 * @param d and p The values to store in the direct and pos fields.
+	 */
+	public void setMovement(int p, String d) {
+		direct = d;
+		pos = p;
+	}
+	
+	/* The method getMovement returns a Drive object's
+	 * pos.
+	 * @return The value in the pos field.
+	 * 
+	 */
+	
+	public int getMovement() {
+		return pos;
+		
+	}
+	
+	/* The method getDirect returns a Drive object's
+	 * direct.
+	 * @return The value in the direct field.
+	 * 
+	 */
+	
+	public String getDirect() {
+		return direct;
+	}
+	
+	/* The method calculateMovement returns a Drive object's
+	 * pos after changing current to the new position.
+	 * @return The value in the pos field.
+	 * 
+	 */
+	
+	public int calculateMovement() {
+		if (direct == "UP") {
+			while (current < pos) {
+				current ++;
+			}
+		}
+		
+		if (direct == "DOWN") {
+			while (current > pos) {
+				current --;
+			}
+		}
+		
+		else {
+			current = 1;
+		}
+		current = pos;
+		return current;
+	}
+	
 }
+
