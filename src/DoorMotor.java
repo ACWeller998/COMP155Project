e703c203a91498ee6247a9c1b121306f54cf4156
MidnightWolfe <36086769+MidnightWolfe@users.noreply.github.com>
2020-03-23 14:40:43 -0700

    Update DoorMotor.java

--- src/DoorMotor.java
+++ src/DoorMotor.java
@@ -1,2 +1,24 @@
 public class DoorMotor {
+  private boolean door;
+	
+	/* The method setDoor stores a value in the field
+	 * door.
+	 * @param d The value to store in the door field. 
+	 */
+
+	public void setDoor(boolean d) {
+		door = d;
+		
+	}
+	
+	
+	/* The method getDoor returns a DoorMotor
+	 * object.
+	 * @return The value in the door field.
+	 * 
+	 */
+	
+	public boolean getDoor() {
+		return door;
+	}
 }
