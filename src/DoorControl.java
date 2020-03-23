374625288ff2d0f5d96b0d9c93af28ac1bccaa03
MidnightWolfe <36086769+MidnightWolfe@users.noreply.github.com>
2020-03-23 14:41:07 -0700

    Update DoorControl.java

--- src/DoorControl.java
+++ src/DoorControl.java
@@ -1,2 +1,23 @@
 public class DoorControl {
+  private boolean buttonDoor; 
+	
+	
+	/*
+	 * The method setButton stores a value in the field
+	 * buttonDoor.
+	 * @param b The value to store in the buttonDoor field. 
+	 */
+	
+	public void setButton(boolean b) {
+		buttonDoor = b;
+	}
+	
+	/*
+	 * The method getButton returns a DoorControl
+	 * object.
+	 * @return The value in the buttonDoor field.
+	 */
+	public boolean getButton() {
+		return buttonDoor;
+	}
 }
