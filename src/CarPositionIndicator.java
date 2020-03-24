74cd19c6ae7ac6eb843542f2447b55b88ccd3a1a
MidnightWolfe <36086769+MidnightWolfe@users.noreply.github.com>
2020-03-24 16:47:27 -0700

    Update CarPositionIndicator.java

--- src/CarPositionIndicator.java
+++ src/CarPositionIndicator.java
@@ -1,2 +1,35 @@
+//@author Kaiya Wangler
 public class CarPositionIndicator {
+	private int floorNum;
+	private int defaultFloor;
+	
+	/* The method setFloorNum stores a value in the floorNum and defaultFloor
+	 * fields.
+	 * @param f and d The values that are stored in the fields floorNum
+	 * and defaultFloor 
+	 */
+
+	public void setFloorNum (int f, int d) {
+		floorNum = f;
+		defaultFloor = d;
+		
+	}
+	
+	/*The method getDefaultFloor returns an object of the class.
+	 * @return The value in the defaultFloor field.
+	 */
+	
+	public int getDefaultFloor() {
+		return defaultFloor;
+	}
+	
+	/*The method getFloorNum returns an object of the class.
+	 * @return The value in the floorNum field. 
+	 */
+	
+	public int getFloorNum() {
+		return floorNum;
+	}
+	
+	
 }
