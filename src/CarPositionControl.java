13a0b2a5d6fe598bd7715fbbbfe8ea8f110ca732
MidnightWolfe <36086769+MidnightWolfe@users.noreply.github.com>
2020-03-27 16:14:56 -0700

    Update CarPositionControl.java

--- src/CarPositionControl.java
+++ src/CarPositionControl.java
@@ -1,37 +1,39 @@
 //@author Kaiya Wangler
-import java.util.Scanner;
 
 public class CarPositionControl {
-	public static void main(String[] args) {
+	private int location;
 	
-	CarPositionIndicator indicator = new CarPositionIndicator();
 	
-	Scanner floor = new Scanner(System.in);
-	System.out.println("Enter a floor 1-3.");
-	int input = floor.nextInt();
+	public void setControlPosition(int l) {
+		location = l;
+	}
 	
-	Scanner maxFloor = new Scanner(System.in);
-	System.out.println("What is the maximum floor of the building?");
-	int max = maxFloor.nextInt();
-	
-	int defFloor = 1;
-	
-	indicator.setFloorNum(input, defFloor);
-	int f = indicator.getFloorNum();
-	int d = indicator.getDefaultFloor();
-	
-	if (f < max && f != d) {
-		while(d <= max) {
-			System.out.println("The floor you are on is " + (d));
-			d++;
-			}		
+	public int getControlPosition() {
+		
+		CarPositionIndicator indicator = new CarPositionIndicator();
+		
+		int defFloor = 1;
+		
+		indicator.setFloorNum(location, defFloor);
+		int f = indicator.getFloorNum();
+		int d = indicator.getDefaultFloor();
+		
+		if (f < 3 && f != d) { //Making this to 3 floors until later (as a placeholder)
+			while(d <= 3) {
+				//System.out.println("The floor you are on is " + (d)); //For testing
+				d++;
+				}		
+			}
+		if (f == d) {
+			System.out.println("This is the ground floor");
+			
 		}
-	if (f == d) {
-		System.out.println("This is the ground floor");
+		
+		return d;
+		
+		
+		//Need a loop to go down
 	}
-//Need a loop to go down.
-
-
 	
-	}
 }
+
