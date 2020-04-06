989050bf1501797a007ce3a4a395f4dd255ae0f1
Jas1921 <63171919+Jas1921@users.noreply.github.com>
2020-04-05 22:53:18 -0700

    Update Lantern.java

--- src/Lantern.java
+++ src/Lantern.java
@@ -16,7 +16,7 @@ public class Lantern {
     //here the lantern will print the current floor and is the next step will be to ask the user about the inout after which the lantern will print msgs accordingly
     
     System.out.println("Please enter the valid floor number");
-    int c_floor = keyboard.readInt();
+    int c_floor = keyboard.readInt();}
     
   public void goTo(int c_Floor){
     if ( c_Floor == floorNumber) {
@@ -45,4 +45,5 @@ public class Lantern {
    
    
     
+}
 }
