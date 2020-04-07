7a04ebf06b71b73643205f5e0b83f2c84e28ade6
Jas1921 <63171919+Jas1921@users.noreply.github.com>
2020-04-06 23:47:21 -0700

    Update Lantern.java

--- src/Lantern.java
+++ src/Lantern.java
@@ -1,49 +1,54 @@
 //Jasnoor
 
-import java.util.Scanner;
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() *Math.floor(max));
-    }
-
 public class Lantern {
-  public static void main(String[] args) {
+    private int input; //floor that user inputted
+    private int current;
+    DriveControl lantern = new DriveControl();
     
-    floorNumber = Console.log(getRandomInt(4));
-    //the output will be : 0,1,2,3 which are the number of floors we have assumed in our elevator.
+ // public void lightdecision() {
+    //if (current < input) {
+// 	driveControl.setPosition(input);
+// 	driveControl.setDirection("UP");
+ // System.out.println (setdirection)
+// 	int p = driveControl.getPosition();
+// 	String d = driveControl.getDirection();
+// 	m.setMovement(p,d);
     
-    System.out.println("The Current floor number" + floorNumber);
+// else if (current > input) {
+// 	driveControl.setPosition(input);
+// 	driveControl.setDirection("DOWN");
+  // System.out.println (setdirection)
+// 	int p = driveControl.getPosition();
+// 	String d = driveControl.getDirection();
+// 	m.setMovement(p,d);
+
+// }
+    
+    OR
+    
+  public void current() {
+      System.out.println("The Current floor number" + currentfloor);
     //here the lantern will print the current floor and is the next step will be to ask the user about the inout after which the lantern will print msgs accordingly
-    
-    System.out.println("Please enter the valid floor number");
-    int c_floor = keyboard.readInt();}
-    
-  public void goTo(int c_Floor){
-    if ( c_Floor == floorNumber) {
-      System.out.println(" You are on the floor of your choice!");}
-    else if ( c_Floor > floorNumber){
-      elevatorsignalUp();}
-    else if ( c_Floor < floorNumber){
-      elevatorsignalDown();}
-      
     }
-  private void elevatorsignalUp(){
-    System.out.println(" ↑ ");//the elevator will show an upward arrow while going up
     
-    while (floorNumber < c_Floor){
-      c_Floor++;
-      System.out.println("You have reached" +c_Floor);}  //for the floor that are continously increasing   
-    System.out.println(c_Floor); //for the floor that has been finally reached as per users choice
+    
+  public void goTo(int currentfloor, int floornumber){
+      if ( currentfloor > floorNumber){
+      elevatorsignalUp();}
+      else if ( c_Floor < floorNumber){
+      elevatorsignalDown();}  
+    }
+    
+    
+  public void elevatorsignalUp(){
+    System.out.println(" ↑ ");//the elevator will show an upward arrow while going up
    
  }
-  private void elevatorsignalDown(){
+    
+  public void elevatorsignalDown(){
     System.out.println(" ↓ ");
-    
-    while ( floorNumber > c_Floor){
-      c_Floor --;
-      System.out.println(c_Floor);}
+    } 
    
-   
-    
 }
-}
+
+
