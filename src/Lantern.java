db7db500152d9e51bef31a75d381c2b54eb08092
Jas1921 <63171919+Jas1921@users.noreply.github.com>
2020-04-08 19:57:13 -0700

    Update Lantern.java

--- src/Lantern.java
+++ src/Lantern.java
@@ -1,53 +1,22 @@
 //Jasnoor
 
 public class Lantern {
-    private int input; //floor that user inputted
-    private int current;
+    
     DriveControl lantern = new DriveControl();
     
- // public void lightdecision() {
-    //if (current < input) {
-// 	driveControl.setPosition(input);
-// 	driveControl.setDirection("UP");
- // System.out.println (setdirection)
-// 	int p = driveControl.getPosition();
-// 	String d = driveControl.getDirection();
-// 	m.setMovement(p,d);
-    
-// else if (current > input) {
-// 	driveControl.setPosition(input);
-// 	driveControl.setDirection("DOWN");
-  // System.out.println (setdirection)
-// 	int p = driveControl.getPosition();
-// 	String d = driveControl.getDirection();
-// 	m.setMovement(p,d);
-
-// }
-    
-    OR
-    
-  public void current() {
-      System.out.println("The Current floor number" + currentfloor);
-    //here the lantern will print the current floor and is the next step will be to ask the user about the inout after which the lantern will print msgs accordingly
+    public void goUp(){
+        
+        driveControl.setPosition;
+        driveControl.setDirection("UP");
+        System.out.println (setDirection);
+    }
+    public void goDown(){
+        driveControl.setPosition;
+        driveControl.setDirection("Down");
+        System.out.println (setDirection);
     }
     
-    
-  public void goTo(int currentfloor, int floornumber){
-      if ( currentfloor > floorNumber){
-      elevatorsignalUp();}
-      else if ( c_Floor < floorNumber){
-      elevatorsignalDown();}  
-    }
-    
-    
-  public void elevatorsignalUp(){
-    System.out.println(" ↑ ");//the elevator will show an upward arrow while going up
-   
- }
-    
-  public void elevatorsignalDown(){
-    System.out.println(" ↓ ");
-    } 
+  
    
 }
 
