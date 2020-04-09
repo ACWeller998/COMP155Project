d2bf78ab4dbc99c44c1151b6a4832f6ea38fa324
Jas1921 <63171919+Jas1921@users.noreply.github.com>
2020-04-08 20:16:08 -0700

    Update LanternControl.java

--- src/LanternControl.java
+++ src/LanternControl.java
@@ -1,2 +1,30 @@
 public class LanternControl {
+  
+  private int current;
+  private int input;
+  
+  
+  
+  public int showFloors() {
+   
+    Drive drive = new Drive;
+    drive.getmovement();
+    System.out.println(getmovement);
+  }
+  
+  public int showStop {
+    
+    Drive drive = new Drive;
+    Drive.calculatemovement();
+    System.out.println ( calculatemovement );
+    
+ }
+  public int upDown(){
+    Lantern lantern = new Lantern;
+    if (current < input) {
+      Lantern.goUp();}
+      
+    else {
+      Lantern.goDown(); }      
+    
 }
