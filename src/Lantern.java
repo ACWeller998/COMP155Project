c66eca0aad76905fb7e664c3709441088cccf3a6
Jas1921 <63171919+Jas1921@users.noreply.github.com>
2020-04-08 20:25:30 -0700

    Update Lantern.java

--- src/Lantern.java
+++ src/Lantern.java
@@ -6,12 +6,12 @@ public class Lantern {
     
     public void goUp(){
         
-        driveControl.setPosition;
+        
         driveControl.setDirection("UP");
         System.out.println (setDirection);
     }
     public void goDown(){
-        driveControl.setPosition;
+       
         driveControl.setDirection("Down");
         System.out.println (setDirection);
     }
