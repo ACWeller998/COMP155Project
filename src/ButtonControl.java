9922b9e608ffb6101cf26bed1e24b14b27020389
Poorrvii <63171823+Poorrvii@users.noreply.github.com>
2020-04-08 18:58:21 -0700

    Update ButtonControl.java

--- src/ButtonControl.java
+++ src/ButtonControl.java
@@ -1,2 +1,13 @@
-public class ButtonControl {
+//author poorvi (300166550)
+
+public class ButtonControl
+{
+    CarButtonControl carButtonControl=new CarButtonControl();
+    HallButtonControl hallButtonControl=new HallButtonControl();
+
+    /* TODO
+    Get the current and destination floors, create an object of this class and call the required methods
+    accordingly to make sure the integration with the ElevatorControl class is fine!
+     */
+
 }
