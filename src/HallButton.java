78f4731858954bc8d0286c8c8c35d490934bd2cf
Poorrvii <63171823+Poorrvii@users.noreply.github.com>
2020-04-08 18:55:57 -0700

    Update HallButton.java

--- src/HallButton.java
+++ src/HallButton.java
@@ -1,6 +1,41 @@
+//author poorvi(300166550)
+
+
 public class HallButton {
 
 
+int currentFloor;
+String direction=null;
+
+
+    public HallButton()
+    {}
+
+    public HallButton(int cfloor, String dirn)
+    {
+    currentFloor=cfloor;
+    direction=dirn;
+    }
+
+    public int getCurrentFloor()
+    {
+        return currentFloor;
+    }
+
+    public void setCurrentFloor(int floor)
+    {
+    currentFloor=floor;
+    }
+
+    public String getDirection()
+    {
+        return direction;
+    }
+
+    public void setDirection(String direct)
+    {
+        direction=direct;
+    }
 
 
 }
