468e3061d92bcb7a5ed700717cc57da8f9be1042
MidnightWolfe <36086769+MidnightWolfe@users.noreply.github.com>
2020-04-09 17:16:51 -0700

    Update CarPositionIndicator.java

--- src/CarPositionIndicator.java
+++ src/CarPositionIndicator.java
@@ -1,34 +1,71 @@
 //@author Kaiya Wangler
+//This class indicates the direction and position of the elevator car.
+//NOT CONNECTED TO MAIN OR DISPATCHER
 public class CarPositionIndicator {
-	private int floorNum;
-	private int defaultFloor;
 	
-	/* The method setFloorNum stores a value in the floorNum and defaultFloor
-	 * fields.
-	 * @param f and d The values that are stored in the fields floorNum
-	 * and defaultFloor 
+	private int position = 1;
+	private boolean direct;
+	private int current = 1;
+	
+	/* The method setPosition stores a value in the fields
+	 * direct and position.
+	 * @param direction and position  The values to store in the direct and position fields.
 	 */
-
-	public void setFloorNum (int f, int d) {
-		floorNum = f;
-		defaultFloor = d;
+	public void setPosition(int position,int max, boolean direction) {
+		direct = direction;
+		this.position = position;
+	}
+	
+	/* The method getPosition returns a CarPositionIndicator object's
+	 * position.
+	 * @return The value in the position field.
+	 * 
+	 */
+	
+	public int getPosition() {
+		return position;
+	}
+	
+	/* The method getDirect returns a CarPositionIndicator object's
+	 * direct.
+	 * @return The value in the direct field.
+	 * 
+	 */
+	
+	public boolean getDirect() {
+		return direct;
+	}
+	
+	/* The method displayPosition displays the position
+	 * and direction that the elevator car is traveling,
+	 * based on the values stored in direct, current, and position.
+	 * 
+	 */
+	
+	public void displayPosition() {
+		if (direct) {
+			while (current < position) {
+				current ++;
+				System.out.println("UP");
+				System.out.println(current);
+			}
+		}
 		
-	}
-	
-	/*The method getDefaultFloor returns an object of the class.
-	 * @return The value in the defaultFloor field.
-	 */
-	
-	public int getDefaultFloor() {
-		return defaultFloor;
-	}
-	
-	/*The method getFloorNum returns an object of the class.
-	 * @return The value in the floorNum field. 
-	 */
-	
-	public int getFloorNum() {
-		return floorNum;
+		if (!direct) {
+			while (current > position) {
+				current --;
+				System.out.println("DOWN");
+				System.out.println(current);
+			}
+		}
+		
+		else {
+			current = 1;
+			System.out.println("UP");
+			System.out.println("1");
+		}
+		//current = position;
+		
 	}
 	
 	
