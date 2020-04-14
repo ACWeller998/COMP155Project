58482667c22307373e04981ae2b2f71f1bf4dfb6
MidnightWolfe <36086769+MidnightWolfe@users.noreply.github.com>
2020-04-14 15:06:22 -0700

    Update Dispatcher.java

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -36,37 +36,6 @@ public class Dispatcher {
 		return doorControl.getDoorStatus();
 	}
 
-//		//System.out.println("Press 'd' to open the door.");
-//		Scanner door = new Scanner(System.in);
-//		String doorIn = door.nextLine();
-//		char answer;
-//		//answer = doorIn.charAt(0);
-//		DoorMotor d  = new DoorMotor();
-//		DoorControl d1 = new DoorControl();
-//		int time = 0; //time for the delay on the door.
-
-		
-//		while (time <= 10) {
-//			if (answer == 'd') {
-//				d1.setButton(true);
-//				boolean c = d1.getButton();
-//				if (c == true) {
-//					d.setDoor(true);
-//					System.out.println("The door is open.");
-//				}
-//
-//			}
-//
-//			if (answer != 'd' || time == 10) {
-//				d1.setButton(false);
-//				boolean c = d1.getButton();
-//				if (c == false) {
-//					d.setDoor(false);
-//					System.out.println("The door is closed.");
-//				}
-//			}
-//			time++;
-//		}
 }
 
 
