a20c0260eb0b521b8633e882c541c05811b0d522
MidnightWolfe <36086769+MidnightWolfe@users.noreply.github.com>
2020-04-14 14:45:45 -0700

    Update CarPositionIndicator.java

--- src/CarPositionIndicator.java
+++ src/CarPositionIndicator.java
@@ -4,6 +4,11 @@
 public class CarPositionIndicator {
 	
 	DriveControl driveControl = new DriveControl();
+	/* The method getFloor returns the position
+	 * of the elevator car.
+	 * @return The floor number of the car.
+	 */
+	
 	
 	public int getFloor() {
 		return driveControl.getPosition(); //This is just the position of the elevator
