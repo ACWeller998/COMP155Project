db1dc3f91c4ab87d0dccc96d6e9bb26b291fc816
Jas1921 <63171919+Jas1921@users.noreply.github.com>
2020-04-14 23:18:21 -0700

    Update HallButtonControl.java

---
+++ src/HallButtonControl.java
@@ -0,0 +1,22 @@
+//jasnoor
+
+public class HallButtonControl {
+    Boolean upLight = false; //not set
+    Boolean downLight = false; // not set
+    
+    HallButton hallButton=new HallButton();
+    
+    public void setHallLight()
+    {
+         if(hallbutton.getDirection().equals("Up"))
+         {
+             upLight=true; //set
+          }
+         if(hallbutton.getDirection().equals("Down")){
+             downLight=true; //set
+             }
+             
+     }
+    }
+             
+             
