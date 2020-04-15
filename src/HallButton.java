e250fb2391dd3e2c3f9ddce1d3305c0a3c7b53f1
Jas1921 <63171919+Jas1921@users.noreply.github.com>
2020-04-14 22:56:07 -0700

    Update HallButton.java

--- src/HallButton.java
+++ src/HallButton.java
@@ -1,6 +1,36 @@
+//Jasnoor Saran
+
 public class HallButton {
-
-
-
-
+  boolean upButton= false;
+  boolean downButton=false;
+  
+  
+  public HallButton(boolean ubutton,boolean dbutton){
+    upButton=ubutton;
+    downButton=dbutton;
+  }
+  
+  
+  public boolean getupButton(){
+    return upButton;
+  }
+  
+  public void setupButton(boolean ubutton){
+    upButton=ubutton;
+  }
+  
+  public boolean getdownButton(){
+    return downButton;
+  }
+  public void setdownButton(boolean dbutton){
+    downButton=dbutton;
+  }
 }
+    
+  
+  
+  
+  
+
+
+
